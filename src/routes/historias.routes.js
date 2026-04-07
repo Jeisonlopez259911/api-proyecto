aef9e2d2107253497adf95abe5d445a6d2454df5
Jeison Stiven Lopez Castro <jeisonlopez2599@gmail.com>
2026-04-07 16:29:24 -0500

    Se corrigio el contenido de los archivos Js de las entidades usuarios e historias de la carpeta router de la api del proyecto

--- src/routes/historias.routes.js
+++ src/routes/historias.routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const historiasControllers = require('../controllers/historias.controllers');
 
-router.post('/usuarios', historiasControllers.crear);
-router.get('/usuarios', historiasControllers.listar);
-router.get('/usuarios/:id', historiasControllers.listarPorId);
-router.put('/usuarios/:id', historiasControllers.actualizar);
-router.delete('/usuarios/:id', historiasControllers.eliminar);
+router.post('/', historiasControllers.crear);
+router.get('/', historiasControllers.listar);
+router.get('/:id', historiasControllers.listarPorId);
+router.put('/:id', historiasControllers.actualizar);
+router.delete('/:id', historiasControllers.eliminar);
 
 module.exports = router;
